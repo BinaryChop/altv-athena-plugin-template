3c75628ef86c58d143a3f35a35d79487e33fc1ea
Nick <101369051+BinaryChop@users.noreply.github.com>
2023-02-19 13:24:33 +0100

    added debug variable

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,10 @@ import { INDEX_CONFIG } from "../shared/config";
 
 PluginSystem.registerPlugin(INDEX_CONFIG.PLUGIN_NAME, () => {
 
+  /* if (!alt.debug) {
+        alt.log(`~lr~CORE ==> ${PLUGIN_NAME} can only be loaded in debug mode.`);
+        return;
+    } */
   alt.log(`~lg~[PLUGIN] ==> ${INDEX_CONFIG.PLUGIN_NAME} V.${INDEX_CONFIG.VERSION} - Athena V.${INDEX_CONFIG.ATHENA_VERSION} successfully loaded.(~w~Authors: ${INDEX_CONFIG.AUTHORS.join(", ")}~lg~)`);
 
 });
